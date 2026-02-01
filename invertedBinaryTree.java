b2ab431531f79815bfdf7ea76672fc73e4e8551c
shuklashweta19 <shwetashukla1907@gmail.com>
2026-02-01 15:29:41 +0530

    Add files via upload

---
+++ invertedBinaryTree.java
@@ -0,0 +1,35 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class invertedBinaryTree {
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+    public static void printTree(TreeNode root) {
+        if (root == null) return;
+        System.out.print(root.val + " ");
+        printTree(root.left);
+        printTree(root.right);
+    }
+}
